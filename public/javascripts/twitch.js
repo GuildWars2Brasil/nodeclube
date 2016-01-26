07f97933516a821bc4c645240d7b0379953f0856
Edmo Bernardes <edmobm@live.com>
2016-01-26 17:40:55 -0200

    Twitch: adicionado um limitador global de streams

--- public/javascripts/twitch.js
+++ public/javascripts/twitch.js
@@ -2,12 +2,15 @@ var gw2api='https://api.twitch.tv/kraken/streams?game=Guild+Wars+2';
 var gw2api_limit= '&limit=5';
 var world= gw2api + gw2api_limit;
 var br= gw2api + '&language=pt' + gw2api_limit;
+var count= 0;
 
 $.getJSON(br, function(data1) {
     $.getJSON(world, function(data2) {
 
         var output="<ul>";
-        for (var i in data1.streams) {
+    while (count < 5) {
+        
+    for (var i in data1.streams) {
             output+='<li class="streamer">' +
             '<span class="name">' + data1.streams[i].channel.display_name + '</span>' +
             '<span class="flag' + data1.streams[i].channel.language + '"></span>' + '</li>'
@@ -15,6 +18,7 @@ $.getJSON(br, function(data1) {
             '<ul><li>' + data1.streams[i].viewers + ' assistindo: <br><strong>' + data1.streams[i].channel.status + '</strong></li>' +
             '<li><a hrf="' + data1.streams[i].channel.url + '" class="preview">' + '<img src="' +
             data1.streams[i].preview.medium + '"></a></li></ul></li>';
+count++;
         }
         for (var i in data2.streams) {
             output+='<li class="streamer">' +
@@ -24,6 +28,8 @@ $.getJSON(br, function(data1) {
             '<ul><li>' + data2.streams[i].viewers + ' assistindo: <br><strong>' + data2.streams[i].channel.status + '</strong></li>' +
             '<li><a hrf="' + data2.streams[i].channel.url + '" class="preview">' + '<img src="' +
             data2.streams[i].preview.medium + '"></a></li></ul></li>';
+count++;
+}
 }
 
         output+="</ul>";
