b53a6e182e427fa35692ad2d34bef5e1d6028229
klarkc <walker@praiseweb.com.br>
2016-01-27 18:42:02 -0300

    BREAKING CHANGE: Alterando versão major do projeto para desvincular do fork.

--- tasks/github-release.js
+++ tasks/github-release.js
@@ -5,7 +5,7 @@ var releaser = require('conventional-github-releaser');
 module.exports = [['checkout-master'], function (done) {
     releaser({
         type: "oauth",
-        token: '7fda***REMOVED***' // change this to your own GitHub token or use an environment variable
+        token: '7fda5d0cb00da5dd9a734749cbe3c6a2a58eccc2' // change this to your own GitHub token or use an environment variable
     }, {
         preset: 'angular' // Or to any other commit message convention you use.
     }, done);
