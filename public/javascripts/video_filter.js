6a1a5c37394db2c5be0d046e1f10700d42516eec
Edmo Bernardes <edmobm@live.com>
2016-01-27 17:50:01 -0200

    temp fix conflito entre video_filter.js e Autolinker.js lib

--- public/javascripts/video_filter.js
+++ public/javascripts/video_filter.js
@@ -1,10 +1,10 @@
 $('.video_filter').html(function(i, html) {
     return html.
-    replace(/(?:http:\/\/|https:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?([^\ ]+)/g,
+    replace(/(?:<a href=")?(?:http:\/\/|https:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?([^\ ]+)?(?:".*\<\/a>)/g,
     '<iframe width="200" height="100" src="http://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>').
-    replace(/(?:http:\/\/|https:\/\/)?(?:www\.)?(?:vimeo\.com)\/([^\ ]+)/g,
+    replace(/(?:<a href=")?(?:http:\/\/|https:\/\/)?(?:www\.)?(?:vimeo\.com)\/([^\ ]+)?(?:".*\<\/a>)/g,
     '<iframe src="//player.vimeo.com/video/$1" width="560" height="315" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>').
-     replace(/(?:http:\/\/|https:\/\/)?(?:www\.)?(?:twitch\.tv)?(?:\/.*v\/)([^\ ]+)/g,
+     replace(/(?:<a href=")?(?:http:\/\/|https:\/\/)?(?:www\.)?(?:twitch\.tv)?(?:\/.*v\/)([^\ ]+)?(?:".*\<\/a>)/g,
     	'<iframe src="http://player.twitch.tv/?video=v$1" frameborder="0" scrolling="no" height="315" width="560"></iframe>');
 
 });
