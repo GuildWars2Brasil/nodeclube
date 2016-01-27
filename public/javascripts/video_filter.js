1ed5a085a3bf8b31178fb3ccfaf943aac663e510
Edmo Bernardes <edmobm@live.com>
2016-01-27 01:44:53 -0200

    fixa um erro ao repor certos links do twitch onde o usuario contem a letra v

--- public/javascripts/video_filter.js
+++ public/javascripts/video_filter.js
@@ -4,7 +4,7 @@ $('.video_filter').html(function(i, html) {
     '<iframe width="560" height="315" src="http://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>').
     replace(/(?:http:\/\/)?(?:www\.)?(?:vimeo\.com)\/([^\ ]+)/g,
     '<iframe src="//player.vimeo.com/video/$1" width="560" height="315" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>').
-     replace(/(?:http:\/\/)?(?:www\.)?(?:twitch\.tv)?(?:\/.*v)\/([^\ ]+)/g,
+     replace(/(?:http:\/\/)?(?:www\.)?(?:twitch\.tv)?(?:\/.*v\/)([^\ ]+)/g,
     	'<iframe src="http://player.twitch.tv/?video=v$1" frameborder="0" scrolling="no" height="315" width="560"></iframe>');
 
 });
