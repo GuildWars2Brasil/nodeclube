936854ce5f2b1170192b1d97b716e45fe9342795
klarkc <walker@praiseweb.com.br>
2016-01-28 11:58:27 -0300

    fix(automation): gulp e task build agora disponíveis no servidor em produção

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,25 @@
 'use strict';
 
-require('gulp-load-tasks')();
+var spawn = require('child_process').spawn;
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+
+// We only need build task on production enviroment
+if(process.env.NODE_ENV !== 'production') {
+  require('gulp-load-tasks')();
+} else {
+  gulp.task('default', ['build']);
+}
+
+gulp.task('build', function (done) {
+  var command = './node_modules/loader-builder/bin/builder',
+    params = ['views', '.'],
+    child = spawn(command, params);
+
+  gutil.log('Running', gutil.colors.cyan(command, params.join(' ')));
+
+  child.stdout.pipe(process.stdout);
+  child.stderr.pipe(process.stderr);
+
+  child.on('exit', done);
+});
