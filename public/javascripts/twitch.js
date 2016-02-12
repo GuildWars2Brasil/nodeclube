dbff221faa0b47be92640663461da828bc8b0603
klarkc <walker@praiseweb.com.br>
2016-02-12 10:38:24 -0300

    fix(twitch): Refatora o código para obedecer patterns do JSLint e incorpora a função docReady

--- public/javascripts/twitch.js
+++ public/javascripts/twitch.js
@@ -1,108 +1,102 @@
-var count = 0;
-var output = "<ul>";
-var array = [];
+(function () {
+  "use strict";
 
-var buildurl = function(lang, limit){
-  return 'https://api.twitch.tv/kraken/streams?game=Guild+Wars+2&language=' + lang
-  + '&limit=' + limit;
-}
+  var
+    buildurl = function (lang, limit) {
+      return "https://api.twitch.tv/kraken/streams?game=Guild+Wars+2&language=" + lang + "&limit=" + limit;
+    },
+    logo = function (obj) {
+      return "<span class=\"logo\" style=\"background-image: url(" + obj.channel.logo + ");\"</span>";
+    },
+    nam = function (obj) {
+      return "<span class=\"name\">" + obj.channel.display_name + "</span>";
+    },
+    flag = function (obj) {
+      return "<span class=\"flag " + obj.channel.language + "\"></span></li>";
+    },
+    views = function (obj) {
+      return "<ul><li>" + obj.viewers + " assistindo: <br>";
+    },
+    title = function (obj) {
+      return "<strong>" + obj.channel.status + "</strong></li>";
+    },
+    image = function (obj, l) {
+      return "<li><a href=\"" + l + "\" class=\"preview\" target=\"_blank\">" + "<img src=\"" +
+        obj.preview.medium + "\"></a></li>";
+    },
+    channelUrl = function (obj) {
+      return obj.channel.url;
+    },
+    url1 = buildurl("pt", 5),
+    url2 = buildurl("en", 5),
+    request1 = new XMLHttpRequest(),
+    request2 = new XMLHttpRequest(),
+    initActive = function () {
+      var streamer = [],
+        details = [];
+      streamer = document.getElementById("twitch").getElementsByClassName("streamer");
+      details = document.getElementById("twitch").getElementsByClassName("details");
+      if (streamer.length > 0) {
+        streamer[0].classList.add("active");
+        details[0].classList.add("list");
+      }
+    },
+    goGo = function (req) {
+      req = req.target;
+      if (req.status >= 200 && req.status < 400) {
+        var data = JSON.parse(req.responseText),
+          twitch = document.getElementById("twitch");
+        twitch.getElementsByTagName("ul")[0].innerHTML = "";
+        data.streams.forEach(function (stream) {
+          var streamer = document.createElement('li'),
+            details = document.createElement('li');
 
-var logo = function(obj){
-  output += '<span class="logo" style="background-image: url('
-    + obj.channel.logo + ');"</span>';
-};
+          streamer.classList.add("streamer");
+          streamer.setAttribute("onclick", "dodajAktywne(this)");
+          streamer.innerHTML = logo(stream);
+          streamer.innerHTML += nam(stream);
+          streamer.innerHTML += flag(stream);
+          twitch.getElementsByTagName("ul")[0].appendChild(streamer);
 
-var nam = function(obj){
-  output += '<span class="name">' + obj.channel.display_name + '</span>';
-};
+          details.classList.add("details");
+          details.innerHTML += views(stream);
+          details.innerHTML += title(stream);
+          details.innerHTML += image(stream, channelUrl(stream));
+          twitch.getElementsByTagName("ul")[0].appendChild(details);
+        });
+        initActive();
+      }
+    };
 
-var flag = function(obj){
-  output += '<span class="flag ' + obj.channel.language + '"></span></li>';
-};
+  request1.open("GET", url1, true);
+  request2.open("GET", url2, true);
 
-var views = function(obj){
-  output += '<ul><li>' + obj.viewers + ' assistindo: <br>';
-};
+  request1.onload = goGo;
+  request2.onload = goGo;
 
-var title = function(obj){
-  output += '<strong>' + obj.channel.status + '</strong></li>'
-};
+  window.docReady(function () {
+    document.getElementById("twitch").innerHTML = "<ul>Carregando...</ul>";
+  });
 
-var image = function(obj, l){
-  output += '<li><a href="' + l + '" class="preview" target="_blank">' + '<img src="' +
-  obj.preview.medium + '"></a></li>';
-};
-
-var channelUrl = function(obj){
-  return obj.channel.url;
-};
-
-var url1 = buildurl('pt', 5);
-var url2 = buildurl('en', 5);
-
-var request1 = new XMLHttpRequest();
-request1.open('GET', url1, true);
-var request2 = new XMLHttpRequest();
-request2.open('GET', url2, true);
-
-var goGo = function(req) {
-  if (req.status >= 200 && req.status < 400) {
-    var data = JSON.parse(req.responseText);
-    for(var i in data.streams){
-      output += '<li class="streamer" onclick="dodajAktywne(this)">';
-      logo(data.streams[i]);
-      nam(data.streams[i]);
-      flag(data.streams[i]);
-      output += '<li class="details">';
-      views(data.streams[i]);
-      title(data.streams[i]);
-      image(data.streams[i], channelUrl(data.streams[i]));
-      output += '</ul></li>';
-      array[count] = output;
-      count++;
-    }
-    document.getElementById("twitch").innerHTML = array[4];
-    initActive();
-  }
-  else{
-
-  }
-};
-
-request1.onload = function(){
-  goGo(request1);
-};
-request2.onload = function(){
-  goGo(request2);
-};
-
-var streamer = [];
-var details = [];
-
- var initActive = function (){
-  streamer = document.getElementById('twitch').getElementsByClassName('streamer');
-  details = document.getElementById('twitch').getElementsByClassName('details');
-
-  streamer[0].classList.add('active');
-  details[0].classList.add('list');
-};
-window.onload = function(){
-  request1.send();
-  request2.send();
+  window.onload = function () {
+    request1.send();
+    request2.send();
   };
 
- // Click events
-function dodajAktywne(elem) {
-    // get all 'a' elements
-    var streamer = document.getElementById('twitch').getElementsByClassName('streamer');
-    var details = document.getElementById('twitch').getElementsByClassName('details');
-    // loop through all 'a' elements
-    for (i = 0; i < streamer.length; i++) {
-        // Remove the class 'active' if it exists
-        streamer[i].classList.remove('active');
-  details[i].classList.remove('list');
+  // Click events
+  window.dodajAktywne = function (elem) {
+    // get all "a" elements
+    var streamer = document.getElementById("twitch").getElementsByClassName("streamer"),
+      details = document.getElementById("twitch").getElementsByClassName("details"),
+      count;
+    // loop through all "a" elements
+    for (count = 0; count < streamer.length; count = count + 1) {
+      // Remove the class "active" if it exists
+      streamer[count].classList.remove("active");
+      details[count].classList.remove("list");
     }
-    // add 'active' classs to the element that was clicked
-    elem.classList.add('active');
-    elem.nextSibling.classList.add('list');
-};
+    // add "active" classs to the element that was clicked
+    elem.classList.add("active");
+    elem.nextSibling.classList.add("list");
+  };
+}());
