59afa095af5387889d6828f98c05680e9af47e19
klarkc <walker@praiseweb.com.br>
2016-02-12 17:37:39 -0300

    fix(tests): Atualiza testes do twitch

--- test/javascripts/twitch.test.js
+++ test/javascripts/twitch.test.js
@@ -1,49 +1,77 @@
 var jsdom = require('mocha-jsdom');
 var fs = require('fs');
+var mockRequest = require('xhr-mock');
 
 describe('test/javascripts/twitch.test.js', function () {
   var $;
   jsdom();
 
-  before(function() {
+  before(function () {
     $ = require('jquery');
-    $.getJSON = function(url, callback){
-      var data = {streams: [
-        {
+    mockRequest.setup();
+
+    var sendData = function (req, res) {
+      var data = {
+        streams: [{
           channel: {
             logo: 'dumb-logo.jpg',
             display_name: 'dumb-name',
-            language: url.indexOf('language') > -1?'pt-br':'en',
+            language: req._url.indexOf('language') > -1 ? 'pt' : 'en',
             status: 'dumb-title',
             url: 'http://dumb-url.com'
           },
           viewers: 999,
-          preview:{medium: 'dumb-preview.jpg'}
-        }
-      ]};
-      callback(data);
+          preview: {
+            medium: 'dumb-preview.jpg'
+          }
+      }]
+      };
+
+      res.status(201);
+      res.body(JSON.stringify(data));
+      return res;
     };
+
+    mockRequest.get("https://api.twitch.tv/kraken/streams?game=Guild+Wars+2&language=pt&limit=5", sendData);
+    mockRequest.get("https://api.twitch.tv/kraken/streams?game=Guild+Wars+2&language=en&limit=5", sendData);
+
   });
 
-  it('should list streams in the right order', function () {
-    document.body.innerHTML = '<div id="twitch"></div>';
+  it('should list streams in the right order', function (done) {
+    var twitch = document.createElement("div");
+    twitch.id = "twitch";
+    document.body.appendChild(twitch);
 
+    var elements = [];
+    var finish = function(element) {
+      elements.push(element);
+      if(elements.length >= 2) {
+        done();
+      } else {
+        Error('Missing elements');
+      }
+    };
+
+    window.document.addEventListener('DOMNodeInserted', function(event){
+      var element = event.target;
+      if(element.className === ' details') {
+        $(element).html().should.containEql('dumb-title');
+        $(element).html().should.containEql('http://dumb-url.com');
+        finish(element);
+      } else if(element.className === ' streamer') {
+        $(element).find('.flag').attr('class').should.equal('flag pt');
+        $(element).html().should.containEql('dumb-logo.jpg');
+        $(element).html().should.containEql('dumb-name');
+        finish(element);
+      }
+    }, false);
+
+    eval(fs.readFileSync('public/libs/docready.js', 'utf8'));
+    eval(fs.readFileSync('public/libs/classlist.js', 'utf8'));
     eval(fs.readFileSync('public/javascripts/twitch.js', 'utf8'));
+  });
 
-    var firstStreamer = $('#twitch .streamer').first();
-    var firstDetail = $('#twitch .details').first();
-    firstStreamer.find('.flag').attr('class').should.equal('flag pt-br');
-    firstStreamer.html().should.containEql('dumb-logo.jpg');
-    firstStreamer.html().should.containEql('dumb-name');
-    firstDetail.html().should.containEql('dumb-title');
-    firstDetail.html().should.containEql('http://dumb-url.com');
-
-    var lastStreamer = $('#twitch .streamer').last();
-    var lastDetail = $('#twitch .details').last();
-    lastStreamer.find('.flag').attr('class').should.equal('flag en');
-    lastStreamer.html().should.containEql('dumb-logo.jpg');
-    lastStreamer.html().should.containEql('dumb-name');
-    lastDetail.html().should.containEql('dumb-title');
-    lastDetail.html().should.containEql('http://dumb-url.com');
+  after(function () {
+    mockRequest.teardown();
   });
 });
