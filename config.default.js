bddddf7566df9f9d528ff1bc18cc26c7043d2a0b
Walker Leite <walker@incuca.com.br>
2017-10-06 12:56:17 -0300

    change port and IP env variables

--- config.default.js
+++ config.default.js
@@ -72,8 +72,8 @@ var config = {
   session_secret: 'gw2brreborn', // 务必修改
   auth_cookie_name: 'gw2brreborn',
 
-  port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
-  ip: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+  port: process.env.PORT || 8080,
+  ip: process.env.IP || '0.0.0.0',
 
   // 话题列表显示的话题数量
   list_topic_count: 20,
